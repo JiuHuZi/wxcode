d7ca47cf28e2609296aa88c5ee67efdb3cef0915
九狐子 <1225247658@qq.com>
2021-09-26 15:56:45 +0800

    update

--- miniprogram/pages/work3/index.js
+++ miniprogram/pages/work3/index.js
@@ -1,66 +1,25 @@
-// miniprogram/pages/work3/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    result: '不知道'
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  submit(e){
+    // console.log(e);
+    var num1 = Number(e.detail.value.num1);
+    var num2 = Number(e.detail.value.num2);
+    console.log(num1,'  ',num2);
+    
+    if(num1 == num2){
+      this.setData({
+        result:'两个数相等'
+      })
+    }else if(num1 > num2){
+      this.setData({
+        result:'第一个数大于第二个数'
+      })
+    }else if(num1 < num2){
+      this.setData({
+        result:'第二个数大于第一个数'
+      })  
+    }
   }
 })
