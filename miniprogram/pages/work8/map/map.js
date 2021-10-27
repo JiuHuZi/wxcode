056af558bac1702990ac2f0688716b26c2ae1473
九狐子 <1225247658@qq.com>
2021-10-27 20:30:22 +0800

    update

--- miniprogram/pages/work8/map/map.js
+++ miniprogram/pages/work8/map/map.js
@@ -1,13 +1,13 @@
 Page({
     data: {
         markers: [{
-            iconPath: "../images/mark.png",
+            // iconPath: "../images/mark.png",
             id: 0,
             latitude: 23.386932,
             longitude: 113.4461,
             title: '图文楼'
         },{
-            iconPath: "../images/mark.png",
+            // iconPath: "../images/mark.png",
             id: 1,
             latitude: 23.383059,
             longitude: 113.44949,
