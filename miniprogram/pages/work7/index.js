619f19e21ab2a6aa1da87b7aeaf2cc34ee9d74b7
九狐子 <1225247658@qq.com>
2021-11-03 14:51:17 +0800

    update

--- miniprogram/pages/work7/index.js
+++ miniprogram/pages/work7/index.js
@@ -1,4 +1,5 @@
-const baseurl = 'https://apis.juhe.cn/simpleWeather/query?key=8e8a514c5fee064bacceb3cc983a22bf&city='
+// const baseurl = 'https://apis.juhe.cn/simpleWeather/query?key=8e8a514c5fee064bacceb3cc983a22bf&city='  //聚合API
+const baseurl = 'https://api.map.baidu.com/weather/v1/?data_type=all&ak=r6s5nOxAG0Fm1yLu0roTC9FQ68WxH3aR&district_id='  //百度API
 Page({
     data:{
         city:"",
@@ -14,15 +15,16 @@ Page({
     search(){
         let city = this.data.city;
         let url = `${baseurl}${city}`;
-        // console.log(url);
+        console.log(url);
         let that = this
         wx.request({
           url,
           success(e){
-            //   console.log(e);
-              let {temperature,direct} = e.data.result.realtime
+              console.log(e);
+              let {temp,wind_dir} = e.data.result.now
               that.setData({
-                temperature,direct
+                temperature:temp,
+                direct:wind_dir
               })
           }
         })
