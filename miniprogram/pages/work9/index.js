fb4a73a0bb07b469c87d921f2437c94c81c4a273
九狐子 <1225247658@qq.com>
2021-11-05 10:47:01 +0800

    update

--- miniprogram/pages/work9/index.js
+++ miniprogram/pages/work9/index.js
@@ -1,3 +1,57 @@
 Page({
-    
+    data:{
+        incometotal:0,
+        paytotal:0,
+        isincome:true,
+        incomelist:[],
+        paylist:[],
+        isinput:true,
+        inputval:''
+    },
+    click(e){
+        console.log(e);
+        if(e.currentTarget.dataset.name == 'income'){
+            this.setData({
+                isincome:true
+            })
+        }else{
+            this.setData({
+                isincome:false
+            })
+        }
+    },
+    add(){
+        this.setData({
+            isinput : !this.data.isinput
+        })
+    },
+    confirm(e){
+        console.log(e);
+        let money = Number(e.detail.value);
+        let incomelist = this.data.incomelist;
+        let paylist = this.data.paylist;
+        if(this.data.isincome){
+            incomelist.push(money)
+            let incometotal = incomelist.reduce((sum,v)=>{
+                return sum += v;
+            },0);
+            this.setData({
+                incomelist,
+                incometotal,
+                inputval:'',
+                isinput:false
+            })
+        }else{
+            paylist.push(money)
+            let paytotal = paylist.reduce((sum,v)=>{
+                return sum += v
+            },0)
+            this.setData({
+                paylist,
+                inputval:'',
+                isinput:false,
+                paytotal
+            })
+        }
+    }
 })
