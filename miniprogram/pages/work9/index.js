3ea528d1617cb65740d31b2755a1a1c4ff2e2e91
九狐子 <1225247658@qq.com>
2021-11-05 11:32:35 +0800

    update

--- miniprogram/pages/work9/index.js
+++ miniprogram/pages/work9/index.js
@@ -1,57 +1,86 @@
 Page({
-    data:{
-        incometotal:0,
-        paytotal:0,
-        isincome:true,
-        incomelist:[],
-        paylist:[],
-        isinput:true,
-        inputval:''
+    data: {
+        incometotal: 0,
+        paytotal: 0,
+        isincome: true,
+        incomelist: [],
+        paylist: [],
+        isinput: true,
+        inputval: ''
     },
-    click(e){
+    click(e) {
         console.log(e);
-        if(e.currentTarget.dataset.name == 'income'){
+        if (e.currentTarget.dataset.name == 'income') {
             this.setData({
-                isincome:true
+                isincome: true
             })
-        }else{
+        } else {
             this.setData({
-                isincome:false
+                isincome: false
             })
         }
     },
-    add(){
+    add() {
         this.setData({
-            isinput : !this.data.isinput
+            isinput: !this.data.isinput
         })
     },
-    confirm(e){
+    confirm(e) {
         console.log(e);
         let money = Number(e.detail.value);
         let incomelist = this.data.incomelist;
         let paylist = this.data.paylist;
-        if(this.data.isincome){
+        if (this.data.isincome) {
             incomelist.push(money)
-            let incometotal = incomelist.reduce((sum,v)=>{
+            let incometotal = incomelist.reduce((sum, v) => {
                 return sum += v;
-            },0);
+            }, 0);
             this.setData({
                 incomelist,
                 incometotal,
-                inputval:'',
-                isinput:false
+                inputval: '',
+                isinput: false
             })
-        }else{
+        } else {
             paylist.push(money)
-            let paytotal = paylist.reduce((sum,v)=>{
+            let paytotal = paylist.reduce((sum, v) => {
                 return sum += v
-            },0)
+            }, 0)
             this.setData({
                 paylist,
-                inputval:'',
-                isinput:false,
+                inputval: '',
+                isinput: false,
                 paytotal
             })
         }
+    },
+    onUnload() {
+        console.log('onUnload');
+        this.onHide()
+    },
+    onHide() {
+        console.log('onHide');
+        let store = [this.data.incomelist, this.data.paylist, this.data.incometotal, this.data.paytotal]
+        wx.setStorage({
+            data: store,
+            key: 'money'
+        })
+    },
+    onLoad(optioin) {
+        console.log('onLoad');
+        let that = this
+        wx.getStorage({
+            key: 'money',
+            success(e) {
+                console.log(e);
+                that.setData({
+                    incomelist: e.data[0],
+                    paylist: e.data[1],
+                    incometotal: e.data[2],
+                    paytotal:e.data[3]
+                })
+            }
+        })
+
     }
 })
