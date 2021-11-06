a8f627a0a91bb866abee4dbb02a0ffe93033bdd2
九狐子 <1225247658@qq.com>
2021-11-06 18:42:07 +0800

    update

--- miniprogram/pages/work7/index.js
+++ miniprogram/pages/work7/index.js
@@ -1,57 +1,129 @@
 // const baseurl = 'https://apis.juhe.cn/simpleWeather/query?key=8e8a514c5fee064bacceb3cc983a22bf&city='  //聚合API
 // const baseurl = 'https://api.map.baidu.com/weather/v1/?data_type=all&ak=r6s5nOxAG0Fm1yLu0roTC9FQ68WxH3aR&district_id='  //百度API
-  //  const baseurl = 'https://devapi.qweather.com/v7/weather/now?key=40d969fc118d4128869ac0375e2e7aac&location='  //和风天气API
-  const baseurl = 'https://devapi.qweather.com/v7/weather/3d?key=40d969fc118d4128869ac0375e2e7aac&location='
+const nowurl = 'https://devapi.qweather.com/v7/weather/now?key=40d969fc118d4128869ac0375e2e7aac&location=' //和风天气API
+const baseurl = 'https://devapi.qweather.com/v7/weather/7d?key=40d969fc118d4128869ac0375e2e7aac&location=';
+const cityurl = 'https://geoapi.qweather.com/v2/city/lookup?key=40d969fc118d4128869ac0375e2e7aac&location='
 Page({
-    data:{
-        city:"",
-        temperature:0,
-        direct:'',
-        latitude:0,
-        longitude:0,
-        wStatus:'晴',
-        humidity:0,
-        wPower:0,
-        kPa:0,
-        weather:[
-          {day:'11-05',maxTemp:'1',minTemp:'1',wType:'晴'},
-          {day:'11-06',maxTemp:'2',minTemp:'2',wType:'晴'},
-          {day:'11-07',maxTemp:'3',minTemp:'3',wType:'晴'}
-        ]
-    },
-    input(e){
-        // console.log(e);
-        this.setData({
-            city:e.detail.value
-        })
-    },
-    search(){
-        let city = this.data.city;
-        let url = `${baseurl}${city}`;
-        let weather = this.data.weather;
-        console.log(url);
-        let that = this
+  data: {
+    isfoot: false,
+    city: "",
+    temperature: 0,
+    direct: '-',
+    latitude: 0,
+    longitude: 0,
+    wStatus: '-',
+    humidity: 0,
+    wPower: 0,
+    kPa: 0,
+    weather: [{
+        day: '',
+        maxTemp: '',
+        minTemp: '',
+        wType: '',
+        ico: 0
+      },
+      {
+        day: '',
+        maxTemp: '',
+        minTemp: '',
+        wType: '',
+        ico: 0
+      },
+      {
+        day: '',
+        maxTemp: '',
+        minTemp: '',
+        wType: '',
+        ico: 0
+      }
+    ]
+  },
+  input(e) {
+
+    let city = e.detail.value;
+    this.setData({
+      city: e.detail.value
+    })
+  },
+  search() {
+    let isfoot = true;
+    let city = this.data.city;
+    let cArr = [];
+    const [adm, loc] = city.split(' ')
+    let url = !loc ? `${cityurl}${adm}` : `${cityurl}${loc}&adm=${adm}`;
+    let latitude = this.data.latitude;
+    let longitude = this.data.longitude;
+    console.log(url);
+    let burl = `${baseurl}${longitude},${latitude}`;
+    console.log(burl);
+    let weather = this.data.weather;
+    let that = this
+    // 用搜索的城市查经纬度
+    wx.request({
+      url,
+      success(e) {
+        console.log(e);
+        let {
+          lat,
+          lon
+        } = e.data.location[0]
+
+        // 近7天天气
         wx.request({
-          url,
-          success(e){
-              console.log(e);
-              let {tempMax,windDirDay,textDay,windScaleDay,pressure,humidity} = e.data.daily[0]
-              for(var i = 0;i<3;i++){
-                weather[i].day = e.data.daily[i].fxDate
-                weather[i].maxTemp = e.data.daily[i].tempMax
-                weather[i].minTemp = e.data.daily[i].tempMin
-                weather[i].wType = e.data.daily[i].textDay
-              }
-              that.setData({
-                temperature:tempMax,
-                direct:windDirDay,
-                wStatus:textDay,
-                wPower:windScaleDay,
-                kPa:pressure,
-                humidity:humidity,
-                weather:weather
-              })
+          url: `${baseurl}${lon},${lat}`,
+          success(e) {
+            console.log(e);
+            let {
+              tempMax,
+              windDirDay,
+              textDay,
+              windScaleDay,
+              pressure,
+              humidity,
+              iconDay
+            } = e.data.daily[0]
+            let index = 0;
+            for (var i = 1; i < 4; i++) {
+              weather[index].day = e.data.daily[i].fxDate
+              weather[index].maxTemp = e.data.daily[i].tempMax
+              weather[index].minTemp = e.data.daily[i].tempMin
+              weather[index].wType = e.data.daily[i].textDay
+              weather[index].ico = e.data.daily[i].iconDay
+              index++
+            }
+            that.setData({
+              weather
+            })
           }
         })
-     }
+
+        wx.request({
+          url: `${nowurl}${lon},${lat}`,
+          success(e) {
+            console.log(e);
+            let {
+              temp,
+              text,
+              humidity,
+              windDir,
+              windScale,
+              pressure
+            } = e.data.now
+            that.setData({
+              temperature: temp,
+              direct: windDir,
+              wStatus: text,
+              wPower: windScale,
+              kPa: pressure,
+              humidity: humidity,
+              isfoot: isfoot
+            })
+          }
+        })
+
+      },
+
+    })
+
+  }
 })
