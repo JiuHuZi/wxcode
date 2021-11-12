778ab24ca84f66487d80d88b7696edf160c3ab98
九狐子 <1225247658@qq.com>
2021-11-12 09:42:29 +0800

    work11

--- miniprogram/pages/work11/index.js
+++ miniprogram/pages/work11/index.js
@@ -1,29 +1,83 @@
 const utils = require('utils');
 Page({
     data: {
-        methers: 100000,
-        seconds: 1000,
-        latitude:0,
-        longitude:0
+        methers: 0,
+        seconds: 0,
+        latitude: 0,
+        longitude: 0,
+        running: false,
+        interval: 1000,
+        markers: []
     },
-    curLocation(){
+    curLocation() {
         wx.getLocation({
-          type:'gcj02'
-        }).then(res=>{
+            type: 'gcj02'
+        }).then(res => {
             console.log(res);
-            let {latitude,longitude} = res
+            let {
+                latitude,
+                longitude
+            } = res
             this.setData({
-                latitude,longitude
+                latitude,
+                longitude
             })
         })
     },
-    onLoad(){
+    onLoad() {
         this.curLocation()
     },
-    test(){
-        let dis = utils.getDistance(this.data.latitude,this.data.longitude,23.383059,113.44949)
+    test() {
+        let dis = utils.getDistance(this.data.latitude, this.data.longitude, 23.383059, 113.44949)
         this.setData({
-            methers:dis
+            methers: dis
         })
+    },
+    run() {
+        this.setData({
+            running: !this.data.running
+        })
+    },
+    record() {
+        if (!this.data.running) {
+            return
+        }
+        this.setData({
+                seconds: this.data.seconds + this.data.interval / 1000
+            }),
+            wx.getLocation({
+                type: 'gcj02',
+            }).then(res => {
+                let newMarker = {
+                    latitude: res.latitude,
+                    longitude: res.longitude,
+                    iconPath: 'img/redpoint.png',
+                    width: 10,
+                    height: 10
+                }
+                let pace = 0
+                let markers = this.data.markers
+
+                if (this.data.markers.length > 0) {
+                    let lastmarker = this.data.markers.slice(-1)[0]
+                    // console.log(lastmarker);
+                    // console.log(newMarker);
+                    pace = utils.getDistance(lastmarker.latitude, lastmarker.longitude, newMarker.latitude, newMarker.longitude)
+                    console.log(pace);
+                    if (pace > 15) {
+                        markers.push(newMarker)
+                    }else{
+                        pace = 10
+                    }
+                }else{
+                    markers.push(newMarker)
+                }
+                this.setData({
+                    latitude:res.latitude,
+                    longitude:res.longitude,
+                    markers:markers,
+                    methers:this.data.methers + pace
+                })
+            })
     }
 })
