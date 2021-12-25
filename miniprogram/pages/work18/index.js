8a58c573bfc6b54164502c2d8373c50dd8ecbd99
九狐子 <1225247658@qq.com>
2021-12-25 11:23:21 +0800

    work18

---
+++ miniprogram/pages/work18/index.js
@@ -0,0 +1,65 @@
+const app = getApp();
+Page({
+  data:{
+    nickName:'',
+    avatarUrl:''
+  },
+  async getuserProfile(){
+    let res = await wx.getUserProfile({
+      desc: '用于完善用户信息',
+    })
+    console.log(res);
+    let {nickName,avatarUrl} = res.userInfo
+    console.log(nickName,avatarUrl);
+    this.setData({
+      nickName,avatarUrl
+    })
+  },
+  async formsubmit(e){
+    console.log(e);
+    let {sn,name} = e.detail.value
+    console.log(sn,name);
+    if(!this.data.nickName){
+      wx.showToast({
+        title: '请获取微信账号信息',
+      })
+      return
+    }
+    if(!sn || !name){
+      wx.showToast({
+        title: '请输入学号姓名',
+      })
+      return
+    }
+    if(!/\d{8}/.test(sn)){
+      wx.showToast({
+        title: '请输入8位学号',
+      })
+      return
+    }
+
+    let nickName = this.data.nickName
+    let avatarUrl = this.data.avatarUrl
+
+    let res = await wx.cloud.callFunction({
+      name:'pc_register',
+      data:{
+        sn,name,nickName,avatarUrl
+      }
+    })
+    console.log('pc_register',res);
+    if(res.result.reg == 'ok'){
+      app.globalData.user = res.result.user
+      wx.showToast({
+        title: '绑定成功',
+      })
+      // 跳转到选择方向页面
+    }else{
+      wx.showModal({
+        cancelColor: 'cancelColor',
+        title:res.result.reg,
+        content:res.result.errMsg
+      })
+    }
+  }
+})
