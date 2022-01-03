ee8d1ad99e63ce122f7ee203d4b573b49aa956ef
九狐子 <1225247658@qq.com>
2022-01-03 18:27:45 +0800

    work18

--- miniprogram/pages/work18/list.js
+++ miniprogram/pages/work18/list.js
@@ -8,7 +8,7 @@ Page({
     directions: [],
     nums: [],
     tab: 0,
-    result: ''
+    result: '',
   },
   async onLoad() {
     // 读取专业方向和限额
@@ -16,39 +16,53 @@ Page({
     let directions = res.data.map(v => {
       return v.name
     })
-    let nums = res.data.map(v=>{
+    let nums = res.data.map(v => {
       return v.num
     })
-    
+
     res = await wx.cloud.callFunction({
-      name:'pc_statistics'
+      name: 'pc_statistics'
     })
     console.log(res);
-    
+
     // 查找本人专业方向
-    let result = res.result.list.find(v=>{
+    let result = res.result.list.find(v => {
       return v.name == app.globalData.user.name
     }).choosen
 
     let tab = directions.indexOf(result)
     let list = []
-    directions.forEach(v=>{
-      list.push(res.result.list.filter(vv=>{
+    
+    directions.forEach(v => {
+      list.push(res.result.list.filter(vv => {
         return vv.choosen == v
       }))
     })
     this.setData({
-      user:app.globalData.user,
+      user: app.globalData.user,
       list,
       result,
       tab,
       directions,
       nums
-    })
+    }) 
+   
   },
-  rank(){
+  rank() {
     wx.redirectTo({
       url: '/pages/work18/rank',
     })
-  }
+  },
+  changeItem(e) {
+    console.log(e);
+    this.setData({
+      tab: e.currentTarget.dataset.item
+    })
+  },
+  changeTab(e) {
+    console.log(e);
+    this.setData({
+      tab: e.detail.current
+    })
+  },
 })
